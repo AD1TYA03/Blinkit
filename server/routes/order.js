95e9c3ff693072aed4ae8a7b388f7a50fc854fc9
ad1tya03 <saaditya17@gmail.com>
2024-08-30 13:19:15 +0530

    routes setup

--- server/routes/order.js
+++ server/routes/order.js
@@ -1,10 +1,19 @@
-import { verifyToken } from "../middleware/auth"
+import { getOrders , createOrder, updateOrderStatus, confirmOrder, getOrderById } from "../controller/order/order.js";
+import { verifyToken } from "../middleware/auth.js"
 
 
 export const orderRouter = async (fastify,options)=>{
     fastify.addHook("prehandler",async (request,reply)=>{
         const isAuthenticated = await verifyToken(request,reply);
         if(!isAuthenticated)
-            return reply.status(401).send({message:"Unauthorized"})
+            return reply.status(401).send({message:"Unauthorized"});
+
     })
+
+    fastify.post('/order',createOrder);
+    fastify.get('/order',getOrders);
+    fastify.patch('/order/:orderId/status',updateOrderStatus)
+    fastify.post('/order/:orderId/confirm',confirmOrder)
+    fastify.get('/order/:orderId',getOrderById)
+
 }
